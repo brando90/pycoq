e936079282bcbbec4672e59313e551786f342d38
Brando Miranda <brandojazz@gmail.com>
2023-06-09 15:10:55 -0700

    small extension for sketch seems to compile and create data

--- lf_coq_project/lf/MyRedBlack.v
+++ lf_coq_project/lf/MyRedBlack.v
@@ -52,7 +52,11 @@ Hypothesis comp_refl_iff :
 
 (* Exercise: *)
 Lemma comp_refl : forall x, comp x x = Eq.
-Proof. Admitted.
+Proof.
+    intros x.
+    apply comp_refl_iff.
+    reflexivity.
+Qed.
 
 (** Red-black trees are binary search trees that contain elements of
     [A] on their internal nodes, and such that every internal node is
@@ -176,162 +180,6 @@ Proof.
     rewrite IH1; trivial.
     rewrite Hfg; trivial.
     rewrite IH2; trivial.
-Qed.
-
-(* Exercise: *)
-Lemma none_occurs :
-  forall (x : A) (f : A -> bool) (t : tree),
-    f x = false ->
-    all f t = true ->
-    occurs x t = false.
-Proof. (* fill in here *) Admitted.
-
-(** With these results, we are ready to prove the correctness of the
-    membership testing algorithm. *)
-
-Lemma member_correct :
-  forall x t,
-    search_tree t = true ->
-    member x t = occurs x t.
-Proof.
-  intros x t.
-  induction t as [|c t1 IH1 y t2 IH2]; simpl; trivial.
-  intros H.
-  destruct (all (fun z => ltb z y) t1) eqn:H1; try discriminate.
-  destruct (all (ltb y) t2) eqn:H2; try discriminate.
-  destruct (search_tree t1) eqn:H3; try discriminate.
-  destruct (search_tree t2) eqn:H4; try discriminate.
-  unfold eqb.
-  rewrite IH1; trivial.
-  rewrite IH2; trivial.
-
-(** We often want to introduce new facts in our context. This can be
-    done with the [assert] tactic.
-
-    New Tactics
-    -----------
-
-    - [assert]: Introduce a new hypothesis in the context, requiring
-      us to prove that it holds. *)
-
-  assert (Hx : ltb x x = false).
-  { unfold ltb. rewrite comp_refl. reflexivity. }
-
-(** The curly braces [{}] allow us to focus on the current subgoal,
-    like [+] and [-]. *)
-
-  destruct (comp x y) eqn:Hxy.
-  - rewrite Bool.orb_true_r. reflexivity.
-  - assert (H2' : all (ltb x) t2 = true).
-    { apply (all_weaken (ltb y) (ltb x)); trivial.
-      intros z.
-      unfold ltb.
-      destruct (comp y z) eqn:Hyz; try congruence.
-      rewrite (comp_trans x y z); trivial. }
-    rewrite (none_occurs x (ltb x) t2 Hx H2').
-    destruct (occurs x t1); reflexivity.
-  - assert (Hxy' : comp y x = Lt).
-    { rewrite comp_opp, Hxy. reflexivity. }
-    assert (H1' : all (fun z => ltb z x) t1 = true).
-    { apply (all_weaken (fun z => ltb z y) (fun z => ltb z x)); trivial.
-      intros z.
-      unfold ltb.
-      destruct (comp z y) eqn:Hyz; try congruence.
-      rewrite (comp_trans z y x); trivial. }
-    rewrite (none_occurs x (fun z => ltb z x) t1); trivial.
-Qed.
-
-Definition tree_color (t : tree) : color :=
-  match t with
-  | Leaf => Black
-  | Node c _ _ _ => c
-  end.
-
-Fixpoint well_colored (t : tree) : bool :=
-  match t with
-  | Leaf => true
-  | Node c t1 _ t2 =>
-
-    let colors_ok :=
-      match c, tree_color t1, tree_color t2 with
-      | Red, Black, Black => true
-      | Red, _, _ => false
-      | Black, _, _ => true
-      end in
-    colors_ok && well_colored t1 && well_colored t2
-  end.
-
-(** The [black_height] function computes the number of black nodes on
-    the path to the left-most leaf of the tree. It is used in the
-    [height_ok] function, which ensures that _all_ paths have the same
-    number of black nodes. *)
-
-Fixpoint black_height (t : tree) : nat :=
-  match t with
-  | Leaf => 0
-  | Node Red t _ _ => black_height t
-  | Node Black t _ _ => S (black_height t)
-  end.
-
-Fixpoint height_ok (t : tree) : bool :=
-  match t with
-  | Leaf => true
-  | Node _ t1 _ t2 =>
-    beq_nat (black_height t1) (black_height t2)
-    && height_ok t1
-    && height_ok t2
-  end.
-
-Definition is_red_black (t : tree) : bool :=
-  well_colored t && height_ok t.
-
-(** The red-black invariant is important because it implies that the
-    height of the tree is logarithmic on the number of nodes. We will
-    now see how to formally show that this is the case. We begin by
-    defining a function [size] for computing various metrics about our
-    trees: *)
-
-Fixpoint size (f : nat -> nat -> nat) (t : tree) : nat :=
-  match t with
-  | Leaf => 0
-  | Node _ t1 _ t2 => S (f (size f t1) (size f t2))
-  end.
-
-(** Note that [size plus] computes the number of elements stored in
-    the tree. [size max] computes the height of the tree, whereas
-    [size min] computes the length of the shortest path from the root
-    of the tree to a leaf.
-
-    As a warm-up exercise, let's show that the black height of a tree
-    is a lower bound on the length of its minimal path: *)
-
-Lemma size_min_black_height :
-  forall t,
-    if height_ok t then black_height t <= size min t
-    else True.
-Proof.
-  intros t.
-  induction t as [|c t1 IH1 x t2 IH2].
-
-  + simpl.
-
-(** To facilitate low-level arithmetic reasoning, we can use the [lia]
-    tactic.
-
-    New Tactics
-    -----------
-
-    - [lia]: Short for "Linear Integer Arithmetic"; tries to solve
-      goals that involve linear systems of inequalites on integers. *)
-
-  lia.
-
-  + simpl.
-    destruct (beq_nat (black_height t1) (black_height t2)) eqn:e12; simpl; trivial.
-    rewrite beq_nat_true_iff in e12.
-    destruct (height_ok t1); simpl; trivial.
-    destruct (height_ok t2); simpl; trivial.
-    destruct c; lia.
 Qed.
 
 (* There is more bellow in the original but I think due to the abort it doesn't work so this is all I put in here
